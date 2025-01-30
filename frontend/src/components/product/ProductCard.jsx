43a7f612c3542bc6407e82cbdf91b446322dbc94
Qamruzzama <qamruzzamakhan1211@gmail.com>
2025-01-30 15:38:38 +0530

    Fix height and width of product card

--- frontend/src/components/product/ProductCard.jsx
+++ frontend/src/components/product/ProductCard.jsx
@@ -1,18 +1,20 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
+
   return (
     <Link to={`/product-details/${product._id}`}>
-      <div className="border border-gray-400 w-fit p-2 rounded">
+      <div className="border border-gray-300 w-fit rounded h-[180px] w-[150px] p-2">
         <img
-          className="h-36 rounded mx-auto"
+          className="h-[110px] w-[120px] mx-auto"
           src={product.image.imageUrl}
           alt="product-image"
         />
-        <p className="text-gray-800 font-semibold">{product.name}</p>
+        <p className="text-gray-800 font-semibold mt-2">{product.name.length < 10 ? product.name : `${product.name.slice(0, 9)}...`}</p>
         <p className="flex items-center gap-1">
           <span className="text-gray-800">Price:</span>
-          <span className="text-gray-600">{product.price}</span>
+          <span className="text-gray-500">{product.price}</span>
           </p>
       </div>
     </Link>
