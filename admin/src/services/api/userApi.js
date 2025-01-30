d9637ffd923de0e56a9fc8b34635c057e657fa63
Qamruzzama-Khan <qamruzzamakhan1211@gmail.com>
2025-01-30 11:28:29 +0530

    Update userApi.js

--- admin/src/services/api/userApi.js
+++ admin/src/services/api/userApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1/auth/user",
+  baseURL: ""https://electromart-backend-vdvm.onrender.com/api/v1/auth/user",
 });
 
 export const loginUser = (data) => api.post("/login", data);
