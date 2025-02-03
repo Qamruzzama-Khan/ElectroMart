9223d99088f74a708f1eaceb81d7e2e0114ad200
Qamruzzama <qamruzzamakhan1211@gmail.com>
2025-02-03 16:59:25 +0530

    Change Url

--- frontend/src/services/api/orderApi.js
+++ frontend/src/services/api/orderApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1/auth/order",
+  baseURL: "https://electromart-backend-o28q.onrender.com/api/v1/auth/order",
 });
 
 // create order
