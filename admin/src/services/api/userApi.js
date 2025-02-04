c84832711f9f919672ee428cba90620f31e2a216
Qamruzzama <qamruzzamakhan1211@gmail.com>
2025-02-04 16:28:03 +0530

    Change Url

--- admin/src/services/api/userApi.js
+++ admin/src/services/api/userApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1/auth/user",
+  baseURL: "https://electromart-backend-o28q.onrender.com/api/v1/auth/user",
 });
 
 export const loginUser = (data) => api.post("/login", data);
