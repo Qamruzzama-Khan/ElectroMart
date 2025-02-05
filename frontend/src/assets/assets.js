28caed517e4b3e17da43e5f2527ef6ea190a7954
Qamruzzama <qamruzzamakhan1211@gmail.com>
2025-02-05 22:24:09 +0530

    Update Ui

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,9 @@
+import hamburger_icon from "./hamburger-icon.png"
+import close_icon from "./close-icon.png"
+import logo from "./logo.svg"
+
+export const assets = {
+    logo,
+    hamburger_icon,
+    close_icon
+}
